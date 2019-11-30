08d08a992651f92bcf31dca8085d6ce809e9581e
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-11-30 15:35:52 +0200

    Create constants and auxiliary functions

---
+++ src/const.js
@@ -0,0 +1,19 @@
+
+export const Colors = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+export const Days = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+export const MonthNames = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
