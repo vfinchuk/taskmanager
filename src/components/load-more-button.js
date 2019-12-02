bade813235a30e2e7d1a54f9cfcc8906e6c332ad
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-12-02 23:17:56 +0200

    Create LoadMoreButton class

--- src/components/load-more-button.js
+++ src/components/load-more-button.js
@@ -1,1 +1,25 @@
+import {createElement} from '../utils.js';
+
 export const createLoadMoreButtonTemplate = () => `<button class="load-more" type="button">load more</button>`;
+
+export default class LoadMoreButton {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createLoadMoreButtonTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
