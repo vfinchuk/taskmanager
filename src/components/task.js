7ab812183f196f923ec8a553b63d8a42b723a95e
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-12-05 23:00:16 +0200

    Add setEditButtonClickHandler() method

--- src/components/task.js
+++ src/components/task.js
@@ -96,4 +96,9 @@ export default class Task extends AbstractComponent {
   getTemplate() {
     return createTaskTemplate(this._task);
   }
+
+  setEditButtonClickHandler(handler) {
+    this.getElement().querySelector(`.card__btn--edit`)
+      .addEventListener(`click`, handler);
+  }
 }
