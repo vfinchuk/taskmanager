28731cce5df083a69a7ee8c19ae91fcc7f4f2bef
Vladimir Finchuk <v.finchuk@lucky-labs.com>
2019-12-08 22:36:36 +0200

    Add sorting types

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,17 +1,28 @@
 import AbstractComponent from './abstract-component';
 
+export const SortType = {
+  DATE_DOWN: `date-down`,
+  DATE_UP: `date-up`,
+  DAFAULT: `default`
+};
+
 const createSortTemplate = () => {
   return (
     `<div class="board__filter-list">
-      <a href="#" class="board__filter">SORT BY DEFAULT</a>
-      <a href="#" class="board__filter">SORT BY DATE up</a>
-      <a href="#" class="board__filter">SORT BY DATE down</a>
+      <a href="#" data-sort-type="${SortType.DAFAULT}" class="board__filter">SORT BY DEFAULT</a>
+      <a href="#" data-sort-type="${SortType.DATE_UP}" class="board__filter">SORT BY DATE up</a>
+      <a href="#" data-sort-type="${SortType.DATE_DOWN}" class="board__filter">SORT BY DATE down</a>
     </div>`
   );
 };
 
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currentSortType = SortType.DAFAULT;
+  }
 
   getTemplate() {
     return createSortTemplate();
